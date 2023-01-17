852e637d791024242906ebd3aa77afbfcf6d2dea
Jack <jack37feus@hotmail.com>
2023-01-17 11:37:11 +0200

    added filter and dropdown functions to app

--- App/app.py
+++ App/app.py
@@ -1,7 +1,7 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output 
 import plotly.express as px
 import pandas as pd
 import CPI_data_clean as cdc
@@ -11,30 +11,41 @@ app = Dash(__name__)
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 data = cdc.cpi_data('../Data/CPI_time_series_December_2022.xls')
-inflation = "GENERAL INDEX (CPI)"
-data = data.loc[data['Index'] == inflation]
+#inflation = "GENERAL INDEX (CPI)"
+#data = data.loc[data['Index'] == inflation]
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-fig1 = px.line(data, x="Date", y="CPI", title='General Inflation - Rwanda', color="Level")
+#fig1 = px.line(data, x="Date", y="CPI", title='General Inflation - Rwanda', color="Level")
+
+##Layout section with components to be displayed
 
 app.layout = html.Div(children=[
     html.H1(children='CPI Dashboard'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
+    dcc.Dropdown(data.Index.unique(), id='inflation', multi=False, value = "GENERAL INDEX (CPI)"),
+    html.Div(id = 'output_container', children=[]),
     dcc.Graph(
-        id='example-graph',
-        figure=fig1
+        id='cpi-fig',
+        figure={}
     )
 ])
+
+@app.callback(
+     [Output(component_id='output_container', component_property='children'),
+     Output(component_id='cpi-fig', component_property='figure')],
+    [Input(component_id='inflation', component_property='value')]
+)
+def update_graph(inflation):
+    print(inflation)
+    print(type(inflation))
+
+    container = "The current CPI index is: {}".format(inflation)
+
+    dff = data.copy()
+    dff = dff.loc[dff["Index"] == inflation]
+    fig = px.line(dff, x="Date", y="CPI", title="{}".format(inflation), color="Level")
+    
+    return container, fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
